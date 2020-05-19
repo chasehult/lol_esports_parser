00401666acf6262bef12dfcb5c837c51cdb78427
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-05-19 12:00:22 +0900

    Ignoring IDE files and comitting a function skeleton.

---
+++ leaguepedia_esports_parser/leaguepedia_esports_parser.py
@@ -0,0 +1,13 @@
+
+def get_game_dto(url: str, get_additional_data=False, translate_ids=False):
+    """Returns the parsed DTO of the game.
+
+    Args:
+        url: the url to parse, pointing to a Riot or QQ endpoint
+        get_additional_data: whether or not to query other existing endpoints to gather more data
+        translate_ids: whether to keep object IDs or to translate them to English names
+
+    Returns:
+        A LolGameDto or LolGameNameDto with all available data.
+    """
+    pass
