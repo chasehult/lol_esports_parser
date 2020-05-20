6ef1f05c8c121cabaffba23dc0d92f4144d692f1
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-05-20 15:33:18 +0900

    Tests update to include "series" objects.

--- leaguepedia_esports_parser/qq_parser.py
+++ leaguepedia_esports_parser/qq_parser.py
@@ -2,20 +2,20 @@ import json
 import logging
 import dateparser
 import urllib.parse as urlparse
-from typing import List
 import requests
 from leaguepedia_esports_parser.config import endpoints
 from leaguepedia_esports_parser.qq_team_name_handler import get_team_name
+import lol_id_tools as lit
 
 
-def get_all_games_dto(qq_match_url: str) -> List[dict]:
-    """Returns a LolGameDto for each game in the match.
+def get_qq_series_dto(qq_match_url: str) -> dict:
+    """Returns a LolSeriesDto.
 
     Params:
         qq_url: the qq url of the full match, usually acquired from Leaguepedia
 
     Returns:
-        A list of LolGameDto for each game in the match.
+        A LolSeriesDto.
     """
     parsed_url = urlparse.urlparse(qq_match_url)
     match_qq_id = urlparse.parse_qs(parsed_url.query)['bmid'][0]
@@ -31,14 +31,14 @@ def get_all_games_dto(qq_match_url: str) -> List[dict]:
     for game in games_list:
         dto_list.append(parse_qq_game(game['sMatchId']))
 
-    return dto_list
+    return {'games': dto_list}
 
 
-def parse_qq_game(qq_game_id: int) -> dict:
+def parse_qq_game(qq_game_id: str) -> dict:
     """Parses a QQ game and returns a LolGameDto.
 
     Params:
-        qq_id: the qq game id, acquired from qq’s match list endpoint
+        qq_id: the qq game id, acquired from qq’s match list endpoint and therefore a string
 
     Returns:
         A LolGameDto.
@@ -50,16 +50,17 @@ def parse_qq_game(qq_game_id: int) -> dict:
         if blue_team == game_info['sMatchInfo']['TeamB'] \
         else game_info['sMatchInfo']['TeamB']
 
-    lol_game_dto = {'qq_id': qq_game_id,
-                    'qq_server': game_info['sMatchInfo']['AreaId'],
-                    'qq_battle_id': game_info['sMatchInfo']['BattleId'],
-                    'qq_tournament': game_info['sMatchInfo']['GameName'],
-                    'game_in_match': game_info['sMatchInfo']['MatchNum'],
+    lol_game_dto = {'qqID': qq_game_id,
+                    'qqServer': game_info['sMatchInfo']['AreaId'],
+                    'qqBattleID': game_info['sMatchInfo']['BattleId'],
+                    'qqTournament': game_info['sMatchInfo']['GameName'],
+                    'gameInMatch': game_info['sMatchInfo']['MatchNum'],
                     'teams': {'blue': {'id': blue_team,
                                        'name': get_team_name(blue_team, game_info)},
                               'red': {'id': red_team,
                                       'name': get_team_name(red_team, game_info)}},
-                    'winner': 'blue' if blue_team == game_info['sMatchInfo']['MatchWin'] else 'red'
+                    'winner': 'blue' if blue_team == game_info['sMatchInfo']['MatchWin'] else 'red',
+                    'players': []
                     }
 
     battle_data = json.loads(game_info['battleInfo']['BattleData'])
@@ -67,24 +68,46 @@ def parse_qq_game(qq_game_id: int) -> dict:
     lol_game_dto['startDate'] = dateparser.parse(f"{battle_data['game-date']} {battle_data['game-time']}")
     lol_game_dto['startTimestamp'] = int(lol_game_dto['startDate'].timestamp() * 1000)
 
-    # TODO check fields names
-    # TODO Parse players at the same time
+    # TODO Check fields names and their meaning
+    # TODO Check how to handle bans
     for team in ['left', 'right']:
         team_side = 'blue' if game_info['sMatchInfo']['TeamA'] == blue_team and team == 'left' else 'red'
 
-        lol_game_dto['teams'][team] = {
-            team_side:
-                {'b-dragon': battle_data[team_dict]['b-dragon'],
-                 's-dragon': battle_data[team_dict]['s-dragon'],
-                 'firstBlood': battle_data[team_dict]['firstBlood'],
-                 'firstTower': battle_data[team_dict]['firstTower'],
-                 'towersKilled': battle_data[team_dict]['tower']}
-            for team_dict in ['left', 'right']}
+        lol_game_dto['teams'][team_side].update({'b-dragon': battle_data[team]['b-dragon'],
+                                                 's-dragon': battle_data[team]['s-dragon'],
+                                                 'firstBlood': battle_data[team]['firstBlood'],
+                                                 'firstTower': battle_data[team]['firstTower'],
+                                                 'towersKilled': battle_data[team]['tower']})
+
+        for player_battle_data in battle_data[team]['players']:
+            lol_game_dto['players'].append(parse_player(player_battle_data, team_side))
 
     return lol_game_dto
 
 
-def get_game_info(qq_game_id: int):
+def parse_player(player_battle_data: dict, team_side) -> dict:
+    output_dict = {'team': team_side,
+                   'kills': player_battle_data['kill'],
+                   'deaths': player_battle_data['death'],
+                   'assists': player_battle_data['assist'],
+                   'gold': player_battle_data['gold'],
+                   'timeDead': player_battle_data['deadTime'],
+                   'inGameName': player_battle_data['name'],
+                   'championId': player_battle_data['hero'],
+                   'championName': lit.get_name(player_battle_data['hero'], object_type='champion'),
+                   'summonerSpells': [],
+                   'items': [],
+                   'runes': []}
+
+    for key in player_battle_data['equip']:
+        output_dict['items'].append({'itemId': player_battle_data['equip'][key],
+                                     'itemName': lit.get_name(player_battle_data['equip'][key], object_type='item'),
+                                     'itemSlot': key[-1]})
+
+    return output_dict
+
+
+def get_game_info(qq_game_id: str):
     game_query_url = f"{endpoints['qq']['match_info']}{qq_game_id}"
 
     logging.info(f'Querying {game_query_url}')
--- leaguepedia_esports_parser/tests/test_parser_qq.py
+++ leaguepedia_esports_parser/tests/test_parser_qq.py
@@ -1,30 +1,45 @@
+import json
+
 import pytest
-from leaguepedia_esports_parser.qq_parser import get_all_games_dto, get_game_info
+from leaguepedia_esports_parser.qq_parser import get_qq_series_dto, get_game_info, parse_player
 
 
 @pytest.fixture
 def game_info():
-    return get_game_info(5346)
+    return get_game_info('5346')
 
 
 def test_get_game_info(game_info):
-    assert int(game_info['sMatchId']) == 5346
+    assert game_info['sMatchId'] == '5346'
+
+
+def test_parse_player(game_info):
+    battle_data = json.loads(game_info['battleInfo']['BattleData'])
+    duke_info = battle_data['left']['players'][0]
+
+    duke = parse_player(duke_info, 'red')
+
+    assert duke['kills'] == 4
+    assert duke['totalCS'] == 237
 
 
 def test_lpl_finals():
     match_url = 'http://lol.qq.com/match/match_data.shtml?bmid=6131'
 
-    dto_list = get_all_games_dto(match_url)
+    series = get_qq_series_dto(match_url)
 
-    lol_game_name_dto = dto_list[4]
+    assert series['winner'] == 'JDG'
+    assert series['score'] == {'JDG': 3, 'TES': 2}
 
-    assert lol_game_name_dto['duration'] == 1679
-    assert lol_game_name_dto['picks_bans'][0]['champion_name'] == 'Zoe'
+    game = series['games'][5]
 
-    assert lol_game_name_dto['players'].__len__() == 10
+    assert game['duration'] == 1679
+    assert game['picksBans'][0]['championName'] == 'Zoe'
 
-    yagao = next(p for p in lol_game_name_dto['players'] if p['role'] == 'mid' and p['team'] == 'red')
+    assert game['players'].__len__() == 10
+
+    yagao = next(p for p in game['players'] if p['role'] == 'mid' and p['team'] == 'red')
 
     assert yagao['kills'] == 4
-    assert yagao['total_cs'] == 184
-    assert yagao['runes']['keystone_name'] == 'Fleet Footwork'
+    assert yagao['totalCS'] == 184
+    assert yagao['runes'][0]['name'] == 'Fleet Footwork'
