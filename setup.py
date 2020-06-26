74d3623e5cdcb839c703765b75a168e1f5601d47
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-06-26 10:58:56 +0900

    a3 + new JSON example.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="lol_esports_parser",
-    version="0.1a2",
+    version="0.1a3",
     packages=setuptools.find_packages(),
     install_requires=["requests", "dateparser", "lol-id-tools>=1.4.0", "riot-transmute>=0.1a4", "lol-dto>=0.1a3"],
     url="https://github.com/mrtolkien/lol_esports_parser",
